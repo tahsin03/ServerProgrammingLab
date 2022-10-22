506864b81cf9f5a84d6ac645ed2adb5133c24aa0
tahsin03 <87164942+tahsin03@users.noreply.github.com>
2022-10-23 01:06:15 +0600

    Lab_04_103

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ router.get("/", homeController.getHome);
 router.get("/book-list", bookController.getBookList);
 router.get("/books", bookController.getBook);
 router.post("/books", bookController.postBook);
-router.get("/delete:id", bookController.deleteBook );
+router.get("/book-list/:id/", bookController.deleteBook );
 router.get("/book-list/:id/edit", bookController.editBook);
 router.post("/book-list/:id", bookController.updateBook);
 
